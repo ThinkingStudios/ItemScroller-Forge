96f5f27385868f22f0bc6824cc953395a6b89fce
TexTrue <3140846162@qq.com>
2023-07-19 19:13:54 +0800

    rename

--- src/main/java/fi/dy/masa/itemscroller/Reference.java
+++ src/main/java/fi/dy/masa/itemscroller/Reference.java
@@ -5,6 +5,6 @@ import fi.dy.masa.malilib.util.StringUtils;
 public class Reference
 {
     public static final String MOD_ID = "itemscroller";
-    public static final String MOD_NAME = "Item Scroller";
+    public static final String MOD_NAME = "Rock'n Roller";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
