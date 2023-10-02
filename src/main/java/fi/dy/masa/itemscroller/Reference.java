a1c46541314b822bf5d77b26d038786210210803
TexTrue <65154269+TexBlock@users.noreply.github.com>
2023-10-02 14:21:54 +0800

    auto upload, change modid to `rocknroller`

--- src/main/java/fi/dy/masa/itemscroller/Reference.java
+++ src/main/java/fi/dy/masa/itemscroller/Reference.java
@@ -4,7 +4,7 @@ import fi.dy.masa.malilib.util.StringUtils;
 
 public class Reference
 {
-    public static final String MOD_ID = "itemscroller";
+    public static final String MOD_ID = "rocknroller";
     public static final String MOD_NAME = "Rock'n Roller";
     public static final String MOD_VERSION = StringUtils.getModVersionString(MOD_ID);
 }
