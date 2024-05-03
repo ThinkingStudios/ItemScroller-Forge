b5c8d72e0a0a3b57658ccbf6fc2ab70b7728397f
TexTrue <65154269+TexBlock@users.noreply.github.com>
2024-05-03 20:20:49 +0800

    update Forge & MaFgLib version

--- src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
+++ src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
@@ -2,10 +2,8 @@ package fi.dy.masa.itemscroller;
 
 import fi.dy.masa.itemscroller.gui.GuiConfigs;
 import fi.dy.masa.malilib.compat.forge.ForgePlatformUtils;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
@@ -15,19 +13,15 @@ public class ItemScroller {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
     public ItemScroller() {
-        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        if (FMLLoader.getDist().isClient()) {
+            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
+            InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
 
-        modEventBus.addListener(this::onInitializeClient);
-    }
-
-    public void onInitializeClient(FMLClientSetupEvent event) {
-        ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
-        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-
-        ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
-            GuiConfigs gui = new GuiConfigs();
-            gui.setParent(screen);
-            return gui;
-        });
+            ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
+                GuiConfigs gui = new GuiConfigs();
+                gui.setParent(screen);
+                return gui;
+            });
+        }
     }
 }
