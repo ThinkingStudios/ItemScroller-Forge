3e1100a27ca9e4f0da97576a23a975f6f423b53b
TexTrue <3140846162@qq.com>
2024-05-23 21:33:37 +0800

    update MaFgLib version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,2 @@
 ## Change
-- update NeoForge & MaFgLib version
+- update MaFgLib version
--- src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
+++ src/main/java/fi/dy/masa/itemscroller/ItemScroller.java
@@ -1,26 +1,14 @@
 package fi.dy.masa.itemscroller;
 
-import fi.dy.masa.itemscroller.gui.GuiConfigs;
-import fi.dy.masa.malilib.compat.neoforge.ForgePlatformUtils;
-import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import fi.dy.masa.malilib.event.InitializationHandler;
 
-@Mod(Reference.MOD_ID)
 public class ItemScroller {
     public static final Logger logger = LogManager.getLogger(Reference.MOD_ID);
 
-    public ItemScroller() {
-        if (FMLLoader.getDist().isClient()) {
-            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
-            InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
-            ForgePlatformUtils.getInstance().getMod(Reference.MOD_ID).registerModConfigScreen((screen) -> {
-                GuiConfigs gui = new GuiConfigs();
-                gui.setParent(screen);
-                return gui;
-            });
-        }
+    public static void onInitialize()
+    {
+        InitializationHandler.getInstance().registerInitializationHandler(new InitHandler());
     }
 }
