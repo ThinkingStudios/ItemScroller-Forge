058ea84cfd14f7f74fb54b709c1fe4277fcd18a9
TexTrue <3140846162@qq.com>
2024-05-23 18:06:22 +0800

    update MaFgLib version

--- src/main/java/fi/dy/masa/itemscroller/gui/GuiConfigs.java
+++ src/main/java/fi/dy/masa/itemscroller/gui/GuiConfigs.java
@@ -18,7 +18,7 @@ public class GuiConfigs extends GuiConfigsBase
 
     public GuiConfigs()
     {
-        super(10, 50, Reference.MOD_ID, null, "itemscroller.gui.title.configs");
+        super(10, 50, Reference.MOD_ID, null, "itemscroller.gui.title.configs", Reference.MOD_NAME, Reference.MOD_VERSION);
     }
 
     @Override
---
+++ src/main/java/org/thinkingstudio/rocknroller/RocknRoller.java
@@ -0,0 +1,34 @@
+package org.thinkingstudio.rocknroller;
+
+import fi.dy.masa.itemscroller.ItemScroller;
+import fi.dy.masa.itemscroller.Reference;
+import fi.dy.masa.itemscroller.event.RenderEventHandler;
+import fi.dy.masa.itemscroller.gui.GuiConfigs;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLLoader;
+import org.thinkingstudio.mafglib.util.ForgePlatformUtils;
+
+@Mod(Reference.MOD_ID)
+public class RocknRoller {
+    public RocknRoller() {
+        if (FMLLoader.getDist().isClient()) {
+            // Make sure the mod being absent on the other network side does not cause
+            // the client to display the server as incompatible
+            ForgePlatformUtils.getInstance().getClientModIgnoredServerOnly();
+            ItemScroller.onInitialize();
+
+            // Config Screen
+            ForgePlatformUtils.getInstance().registerModConfigScreen(Reference.MOD_ID, (screen) -> {
+                GuiConfigs gui = new GuiConfigs();
+                gui.setParent(screen);
+                return gui;
+            });
+
+            MinecraftForge.EVENT_BUS.<GuiScreenEvent.DrawScreenEvent.Post>addListener(event -> {
+                RenderEventHandler.instance().onDrawScreenPost(event.getGui().getMinecraft());
+            });
+        }
+    }
+}
